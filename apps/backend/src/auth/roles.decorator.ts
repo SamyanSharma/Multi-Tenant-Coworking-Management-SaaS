25f518e274068dcb606f9039b2458ad9fcd31720
arpit-bot810 <arpitkhanal111@gmail.com>
2026-08-05 23:29:30 +0545

    Update roles.decorator.ts

--- apps/backend/src/auth/roles.decorator.ts
+++ apps/backend/src/auth/roles.decorator.ts
@@ -1,1 +1,10 @@
+import { SetMetadata } from '@nestjs/common';
 
+export enum Role {
+  PLATFORM_ADMIN = 'PLATFORM_ADMIN',
+  SPACE_MANAGER = 'SPACE_MANAGER',
+  MEMBER = 'MEMBER',
+}
+
+export const ROLES_KEY = 'roles';
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
