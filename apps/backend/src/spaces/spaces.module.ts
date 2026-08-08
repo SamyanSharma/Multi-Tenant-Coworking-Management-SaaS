268678fa7376f1532d470313e0bbc780c67f56e9
arpit-bot810 <arpitkhanal111@gmail.com>
2026-08-08 19:01:59 +0545

    Update spaces.module.ts

--- apps/backend/src/spaces/spaces.module.ts
+++ apps/backend/src/spaces/spaces.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SpacesController } from './spaces.controller';
 import { SpacesService } from './spaces.service';
-import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [SpacesController],
   providers: [SpacesService],
 })
